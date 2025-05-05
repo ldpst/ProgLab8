63ffa920ce23d907f78006b66914c2efcfd46937
ldpst <lazydolphins@mail.ru>
2025-05-06 01:02:19 +0300

    add test interface

---
+++ client/src/main/java/client/InterfaceMain.java
@@ -0,0 +1,9 @@
+package client;
+
+import client.managers.InterfaceManager;
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+        new InterfaceManager().run();
+    }
+}
