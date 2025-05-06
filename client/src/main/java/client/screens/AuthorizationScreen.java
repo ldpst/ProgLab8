43e82385204eaed4edf261001310bdc243c12dd2
ldpst <lazydolphins@mail.ru>
2025-05-06 20:39:43 +0300

    made clean code

--- client/src/main/java/client/screens/AuthorizationScreen.java
+++ client/src/main/java/client/screens/AuthorizationScreen.java
@@ -6,132 +6,180 @@ import client.utils.Languages;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class AuthorizationScreen {
+    private JFrame frame;
+    private JTextField loginTextField;
+    private FocusListener loginFocusListener;
+    private JTextField passwordTextField;
+    private FocusListener passwordFocusListener;
+    private JButton logInButton;
+    private JButton signUpButton;
+
     public AuthorizationScreen() {
 
     }
 
     public void run() {
-        JFrame frame = new JFrame();
+        initializeFrame();
+        frame.add(buildMainPanel());
+        frame.setVisible(true);
+        SwingUtilities.invokeLater(frame::requestFocusInWindow); // для снятия фокуса с loginTextField
+    }
+
+    private void initializeFrame() {
+        frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle(Languages.get("authorizationScreen"));
         frame.setSize(600, 400);
+        frame.setMinimumSize(new Dimension(339, 202));
+    }
 
-        JPanelDeb languagePanel = getLanguagePanel();
+    private JPanelDeb buildMainPanel() {
+        JPanelDeb mainPanel = new JPanelDeb(new BorderLayout());
+        mainPanel.setBorder(new LineBorder(Color.PINK, 2));
+        mainPanel.add(buildLanguagePanel(), BorderLayout.PAGE_START);
+        mainPanel.add(buildCenterPanel(), BorderLayout.CENTER);
+        return mainPanel;
+    }
 
-        JPanelDeb panelLoginPassword = getLoginPasswordPanel();
+    private JPanelDeb buildLanguagePanel() {
+        JPanelDeb panel = new JPanelDeb();
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        panel.setBorder(new LineBorder(Color.GREEN));
+        panel.add(buildLanguageComboBox());
+        return panel;
+    }
 
-        JPanelDeb panelButtons = getButtonPanel();
+    private JComboBox<String> buildLanguageComboBox() {
+        JComboBox<String> comboBox = new JComboBox<>(Languages.getLangs());
+        comboBox.setSelectedItem(Languages.getLanguage());
+        comboBox.addActionListener(e -> {
+            int selectedLanguage = comboBox.getSelectedIndex();
+            Languages.setLanguage(Languages.getLocalNames()[selectedLanguage]);
+            updateLocal();
+        });
+        return comboBox;
+    }
+
+    private JPanelDeb buildCenterPanel() {
+        JPanelDeb outerPanel = new JPanelDeb(new GridBagLayout());
+        outerPanel.setBorder(new LineBorder(Color.YELLOW, 2));
 
         JPanelDeb joinPanel = new JPanelDeb();
         joinPanel.setLayout(new BoxLayout(joinPanel, BoxLayout.Y_AXIS));
         joinPanel.setBorder(new LineBorder(Color.BLUE, 2));
-        joinPanel.add(panelLoginPassword);
-        joinPanel.add(panelButtons);
+        joinPanel.add(buildLoginPasswordPanel());
+        joinPanel.add(buildButtonPanel());
 
-        JPanelDeb outerPanel = new JPanelDeb();
-        outerPanel.setLayout(new GridBagLayout());
-        outerPanel.setBorder(new LineBorder(Color.YELLOW, 2));
         outerPanel.add(joinPanel);
-
-        JPanelDeb mainPanel = new JPanelDeb();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBorder(new LineBorder(Color.PINK, 2));
-        mainPanel.add(languagePanel, BorderLayout.PAGE_START);
-        mainPanel.add(outerPanel, BorderLayout.CENTER);
-
-        frame.add(mainPanel);
-        frame.setVisible(true);
-
-        SwingUtilities.invokeLater(frame::requestFocusInWindow); // для снятия фокуса с loginTextField
+        return outerPanel;
     }
 
-    private JPanelDeb getLanguagePanel() {
-        JPanelDeb panel = new JPanelDeb();
-        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
-        panel.setBorder(new LineBorder(Color.GREEN));
-
-        JComboBox<String> languageComboBox = getLanguageComboBox();
-        panel.add(languageComboBox);
-
-        return panel;
-    }
-
-    private JComboBox<String> getLanguageComboBox() {
-        String[] languages = Languages.getLangs();
-        JComboBox<String> comboBox = new JComboBox<>(languages);
-
-        return comboBox;
-    }
-
-    private JPanelDeb getLoginPasswordPanel() {
+    private JPanelDeb buildLoginPasswordPanel() {
         JPanelDeb panel = new JPanelDeb();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(new LineBorder(Color.RED, 2));
 
-        JTextField loginTextField = getTextField(Languages.get("login"));
+        loginTextField = buildTextField("login");
         panel.add(loginTextField);
         panel.add(Box.createRigidArea(new Dimension(0, 1)));
 
-        JTextField passwordTextField = getTextField(Languages.get("password"));
+        passwordTextField = buildTextField("password");
         panel.add(passwordTextField);
         panel.add(Box.createRigidArea(new Dimension(0, 1)));
 
         return panel;
     }
 
-    private JTextField getTextField(String name) {
+    private JTextField buildTextField(String key) {
         JTextField textField = new JTextField(16);
-        textField.setText(name);
-        textField.setForeground(Color.GRAY);
+        setPlaceholder(textField, key);
 
         Dimension size = new Dimension(300, 30);
         textField.setMaximumSize(size);
         textField.setPreferredSize(size);
         textField.setMinimumSize(size);
 
-        textField.addFocusListener(new FocusAdapter() {
-            public void focusLost(FocusEvent e) {
-                if (textField.getText().isEmpty()) {
-                    textField.setForeground(Color.GRAY);
-                    textField.setText(name);
-                }
-            }
-
-            public void focusGained(FocusEvent e) {
-                if (textField.getText().equals(name)) {
-                    textField.setForeground(Color.BLACK);
-                    textField.setText("");
-                }
-            }
-        });
+        FocusListener focusListener = buildFocusListener(textField, key);
+        textField.addFocusListener(focusListener);
+        if (key.equals("login")) loginFocusListener = focusListener;
+        else passwordFocusListener = focusListener;
 
         return textField;
     }
 
-    private JPanelDeb getButtonPanel() {
+    private void setPlaceholder(JTextField textField, String key) {
+        textField.setText(Languages.get(key));
+        textField.setForeground(Color.GRAY);
+    }
+
+    private FocusListener buildFocusListener(JTextField textField, String name) {
+        return new FocusAdapter() {
+            final String text = Languages.get(name);
+            public void focusGained(FocusEvent e) {
+                if (textField.getText().equals(text)) {
+                    textField.setForeground(Color.BLACK);
+                    textField.setText("");
+                }
+            }
+
+            public void focusLost(FocusEvent e) {
+                if (textField.getText().isEmpty()) {
+                    textField.setForeground(Color.GRAY);
+                    textField.setText(text);
+                }
+            }
+        };
+    }
+
+    private JPanelDeb buildButtonPanel() {
         JPanelDeb panel = new JPanelDeb();
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
         panel.setBorder(new LineBorder(Color.BLACK, 2));
 
-        JButton logIn = getButton(Languages.get("logIn"));
-        panel.add(logIn);
+        logInButton = buildButton("logIn");
+        signUpButton = buildButton("signUp");
 
-        JButton signUp = getButton(Languages.get("signUp"));
-        panel.add(signUp);
+        panel.add(logInButton);
+        panel.add(Box.createRigidArea(new Dimension(1, 0)));
+        panel.add(signUpButton);
 
         return panel;
     }
 
-    private JButton getButton(String label) {
-        JButton button = new JButton(label);
-        Dimension size = new Dimension(150, 30);
+    private JButton buildButton(String key) {
+        JButton button = new JButton(Languages.get(key));
+        Dimension size = new Dimension(148, 30);
         button.setPreferredSize(size);
         button.setMaximumSize(size);
         button.setMinimumSize(size);
         return button;
+    }
+
+    private void updateLocal() {
+        frame.setTitle(Languages.get("authorizationScreen"));
+
+        updateTextField(loginTextField, loginFocusListener, "login");
+        loginFocusListener = buildFocusListener(loginTextField, "login");
+        loginTextField.addFocusListener(loginFocusListener);
+
+        updateTextField(passwordTextField, passwordFocusListener, "password");
+        passwordFocusListener = buildFocusListener(passwordTextField, "password");
+        passwordTextField.addFocusListener(passwordFocusListener);
+
+        logInButton.setText(Languages.get("logIn"));
+        signUpButton.setText(Languages.get("signUp"));
+    }
+
+    private void updateTextField(JTextField textField, FocusListener focusListener, String key) {
+        textField.removeFocusListener(focusListener);
+        if (textField.getForeground().equals(Color.GRAY)) {
+            textField.setText(Languages.get(key));
+        }
     }
 }
--- client/src/main/java/client/utils/Languages.java
+++ client/src/main/java/client/utils/Languages.java
@@ -6,10 +6,13 @@ import java.util.Map;
 public class Languages {
     private static final Map<String, Class<? extends Enum<?>>> languages = new HashMap<>();
     private static final String[] langs = {"Русский", "Slovenski", "Français", "Español (nicaragua)"};
+    private static final String[] localNames = {"Russian", "Slovenian", "French", "Spanish"};
 
     static {
         languages.put("Russian", Russian.class);
         languages.put("Slovenian", Slovenian.class);
+        languages.put("French", French.class);
+        languages.put("Spanish", Spanish.class);
     }
 
     private static String language = "Russian";
@@ -38,11 +41,54 @@ public class Languages {
     }
 
     private enum Slovenian {
-        authorizationScreen("Zaslon za avtorizacijo");
+        authorizationScreen("Zaslon za avtorizacijo"),
+        login("Prijava"),
+        password("Geslo"),
+        logIn("Vstopite"),
+        signUp("Registracija"),
+        ;
 
         final String string;
 
         Slovenian(String string) {
+            this.string = string;
+        }
+
+        public String toString() {
+            return string;
+        }
+    }
+
+    private enum French {
+        authorizationScreen("Écran d'autorisation"),
+        login("Se connecter"),
+        password("Mot de passe"),
+        logIn("Entrer"),
+        signUp("Inscription"),
+        ;
+
+        final String string;
+
+        French(String string) {
+            this.string = string;
+        }
+
+        public String toString() {
+            return string;
+        }
+    }
+
+    private enum Spanish {
+        authorizationScreen("Pantalla de autorización"),
+        login("Acceso"),
+        password("Contraseña"),
+        logIn("Ingresar"),
+        signUp("Registro"),
+        ;
+
+        final String string;
+
+        Spanish(String string) {
             this.string = string;
         }
 
@@ -65,7 +111,15 @@ public class Languages {
         Languages.language = language;
     }
 
+    public static String getLanguage() {
+        return Languages.language;
+    }
+
     public static String[] getLangs() {
         return langs;
+    }
+
+    public static String[] getLocalNames() {
+        return localNames;
     }
 }
